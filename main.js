07f9e49b605d2896b0244d7da0992762d8439a34
germanter <usersuer32@gmail.com>
2026-01-02 01:25:44 +0400

    fef

--- main.js
+++ main.js
@@ -3,13 +3,11 @@
 import { renderGames } from './global.js';
 
 
-// Panel Kontrol Fonksiyonu
 function setupEmailPanel() {
     const btn = document.getElementById('sendItBtn');
     const panel = document.getElementById('emailPanel');
     const close = document.getElementById('closePanel');
 
-    // Elementlerin sayfada olup olmadığını kontrol edelim (hata almamak için)
     if (btn && panel && close) {
         btn.addEventListener('click', () => {
             panel.classList.remove('hidden');
@@ -23,7 +21,6 @@ function setupEmailPanel() {
     }
 }
 
-// DOĞRU KULLANIM: Tek bir fonksiyon içinde ikisini de çağır
 document.addEventListener('DOMContentLoaded', () => {
     setupEmailPanel();
     renderGames("gamesGrid"); 
